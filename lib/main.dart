1a413185a8f4eef8007af45a7043ea9aecc7a370
AurelioAlfons <yuroalfons0407@gmail.com>
2024-09-24 13:00:05 +1000

    Added the navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Aurelio Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 255, 190, 12),
           title: const Text("Aurelio Flutter Demo"),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            // ignore: avoid_print
+            print('pressed!');
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          )
+        ]),
         body: const Center(
-          child: Column(
+          child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(
-                "Hello, Welcome to Aurelio's UI 🐲",
+                "🐦‍🔥 Hello, Welcome to Aurelio's UI 🐲",
                 style: TextStyle(fontSize: 28),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 28),
               Icon(Icons.backpack),
               Icon(Icons.leaderboard),
               Icon(Icons.person),
